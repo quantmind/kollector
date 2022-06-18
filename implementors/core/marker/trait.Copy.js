3a5f7e3a04e55ebed37616595af841d1598e235c
lsbardel <luca@quantmind.com>
2022-06-18 07:26:15 +0000

    Deploying to gh-pages from @ quantmind/kollector@46daaf3297992e4eacaae2640d3ed018a0a1c963 🚀

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["common"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"common/enum.ErrorKind.html\" title=\"enum common::ErrorKind\">ErrorKind</a>","synthetic":false,"types":["common::http::ErrorKind"]}];
+implementors["common"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"common/enum.ErrorKind.html\" title=\"enum common::ErrorKind\">ErrorKind</a>","synthetic":false,"types":["common::http::ErrorKind"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
